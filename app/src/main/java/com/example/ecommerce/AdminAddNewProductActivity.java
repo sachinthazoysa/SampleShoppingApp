d726940bb39834d34f35bef57685081adf86bae5
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-04-09 16:32:27 +0530

    add new product interface implemented

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -3,22 +3,34 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private Button AddNewProductButton;
+    private EditText InputProductName, InputProductDescription,InputProductPrice;
+    private ImageView InputProductImage;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
-        Toast.makeText(this,"Welcome admin",Toast.LENGTH_LONG).show();
+
+        AddNewProductButton = (Button)findViewById(R.id.add_new_product);
+        InputProductImage=(ImageView)findViewById(R.id.select_product_image);
+        InputProductName=(EditText)findViewById(R.id.product_name);
+        InputProductDescription=(EditText)findViewById(R.id.product_description);
+        InputProductPrice=(EditText)findViewById(R.id.product_price);
 
         categoryName = getIntent().getExtras().get("category").toString();
 
-        Toast.makeText(this,categoryName,Toast.LENGTH_LONG).show();
+
     }
 
 }
