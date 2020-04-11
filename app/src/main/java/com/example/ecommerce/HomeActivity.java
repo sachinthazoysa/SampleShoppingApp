fe1d0950a0da60d1906d093cc4f0b347f08924f9
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-04-11 18:36:01 +0530

    Created the cart database. Can add items to the cart now

--- app/src/main/java/com/example/ecommerce/HomeActivity.java
+++ app/src/main/java/com/example/ecommerce/HomeActivity.java
@@ -111,12 +111,20 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
                         holder.txtProductName.setText(model.getPname());
                         holder.txtProductDescription.setText(model.getDescription());
                         holder.txtPrice.setText("Price = " +model.getPrice()+"Rupees.");
                         Picasso.get().load(model.getImage())
                                 .into(holder.imageView);
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(HomeActivity.this,ProductDetailsActivity.class);
+                                 intent.putExtra("pid",model.getPid()) ;
+                                startActivity(intent);
+                            }
+                        });
                     }
 
                     @NonNull
